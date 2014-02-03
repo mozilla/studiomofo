e3082ad63bcebe57b61258b37153afba756a37e4
Mavis Ou <mmmavisss@gmail.com>
2014-02-03 16:44:50 -0500

    git init the app

---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var handlebars = require("handlebars");
+var cons = require("consolidate"); // template engine consolidation library
+var http = require("http");
+var fs = require('fs');
+var path = require('path');
+var xml2js = require("xml2js");
+
+var app = express();
+
+app.configure(function(){
+    app.set("view engine", "html");
+    app.set("view options", { layout: false });
+    app.set("views", __dirname + "/views");
+    app.engine("html", cons.handlebars);
+    app.use(express.static(__dirname + "/public"));
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+});
+
+var viewdir = path.join(__dirname, 'views');
+fs.readdirSync(viewdir).forEach(function(filename){
+    var ext = path.extname(filename);
+    if (filename[0] === '_' && ext === '.html'){
+        var filepath = path.join(viewdir, filename);
+        var stringtemplate = fs.readFileSync(filepath, 'utf8');
+        var templatename = path.basename(filename, ext).slice(1);
+        handlebars.registerPartial(templatename, stringtemplate);
+        //console.log('registering %s partial: %s characters', templatename, stringtemplate.length);
+    }
+});
+
+
+app.get('/', function(req, res){
+  // res.send('Hello World');
+  res.render("index");
+});
+
+app.listen(process.env.PORT, function() {
+    console.log("Listening on " + process.env.PORT);
+});
